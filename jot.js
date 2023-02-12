bfd0494e380017a409ba603c06257d00aa46beae
Juan Patten <jrp@maild.name>
2023-02-12 11:32:04 -0800

    Scroll syncing

--- jot.js
+++ jot.js
@@ -6,6 +6,10 @@ ET_p.on = ET_p.addEventListener; ET_p.off = ET_p.removeEventListener
 ////////////////////////////////////////////////////////////////////////
 
 
+const $input = $('#input')
+const $output = $('#output')
+
+
 const math = Object.fromEntries(
   Object.getOwnPropertyNames(Math).map(k => [k, Math[k]]))
 
@@ -14,8 +18,7 @@ const esc = x => x.replaceAll('"', '\\x22')
 
 function eval_input() {
   let __out = []
-  __out.code = $('#input')
-    .value
+  __out.code = $input.value
     .split('\n')
     .map((x,i) => `try {__out[${i}] = eval("${esc(x)}")} catch {}`)
     .join('\n')
@@ -25,8 +28,8 @@ function eval_input() {
     set(_,k,v){ _[k] = v }
   })
   with(p) { eval(__out.code) }
-  $('#output').innerText = __out.join('\n')
-  localStorage.setItem('input', $('#input').value)
+  $output.innerText = __out.join('\n') + '\n'
+  localStorage.setItem('input', $input.value)
 }
 
 
@@ -34,17 +37,38 @@ function eval_input() {
 
 
 let eval_timer = null
+let scroll_timer = null
 
-$('#input').on('input', e => {
+$input.on('input', e => {
   clearTimeout(eval_timer)
   eval_timer = setTimeout(eval_input, 250)
 })
 
 
+function handle_scroll(elem, fn) {
+  clearTimeout(scroll_timer)
+  scroll_timer = setTimeout(() => { elem.on('scroll', fn) }, 100)
+}
+
+function input_scroll(e) {
+  $output.off('scroll', output_scroll)
+  $output.scrollTop = $input.scrollTop
+  handle_scroll($output, output_scroll)
+}
+$input.on('scroll', input_scroll)
+
+function output_scroll(e) {
+  $input.off('scroll', input_scroll)
+  $input.scrollTop = $output.scrollTop
+  handle_scroll($input, input_scroll)
+}
+$output.on('scroll', output_scroll)
+
+
 ////////////////////////////////////////////////////////////////////////
 
 
-$('#input').value = localStorage.getItem('input') || `
+$input.value = localStorage.getItem('input') || `
 // Use comments for notes
 
 123           // lines are evaluated
@@ -58,6 +82,7 @@ b = log10(a)  // and use Math keys
 `.trim()
 
 eval_input()
+
 
 
 
